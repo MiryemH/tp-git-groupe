015f4c56de107d2d3b6695d4d311bdab8cd06313
jurufola <moulaye.haidara@laposte.net>
2021-08-17 14:33:53 +0200

    Creation composant timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyTimerComponent } from './my-timer/my-timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTimerComponent
   ],
   imports: [
     BrowserModule,
